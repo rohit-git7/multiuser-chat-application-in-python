5ceac0276964e1613f4c27d60b5fe921a2856847
Rohit Takhar <rohit.takhar1994@gmail.com>
2015-02-02 23:58:45 +0530

    Update client.py

--- client.py
+++ client.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python
-import socket
+import socket 
 import select
 import sys
+#creating socket
 try:
    sd = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 except:
    print "Error in creating socket"
    sys.exit(1)
-
+#host and port num are given through commandline arguments
 host = sys.argv[1]
 port = int(sys.argv[2])
 
@@ -18,15 +19,18 @@ except:
    sys.exit(1)
 print "Connected to host"
 while True:
+   #maintaining list of descriptors who can read
    socket_list = [sys.stdin,sd]
+   #select call to select the descriptor that is ready to read
    read_list,write_list,error_list = select.select(socket_list,[],[])
-   
+   #to check which descriptor is ready and take appropriate action accordingly
    for sock in read_list:
 	if sock is sd:
 		message = sys.stdin.readline()
 		sd.send(message)
 	else:
 		data = sd.recv(1024)
+		# no data means server has disconnected
 		if not data:
 			sys.stdout.write("Connection closed")
 			sys.stdout.flush()
